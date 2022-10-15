9cc37be12db0245b24810208c92de1b10b976ef4
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-15 06:04:04 +0000

    Adding tarfile member sanitization to extractall()

--- runway/utils.py
+++ runway/utils.py
@@ -124,7 +124,26 @@ def extract_tarball(path):
         def encode_ascii(member):
             member.name = unidecode(member.name)
         [encode_ascii(member) for member in tar.getmembers()]
-        tar.extractall(path=extracted_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path=extracted_dir)
     return extracted_dir
 
 
